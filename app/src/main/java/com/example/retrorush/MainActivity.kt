3370eef24e35bed09283e3187c01537ec4359608
Pranjal <mpranjal05@gmail.com>
2022-02-08 23:26:18 +0530

    Setup the Api call

--- app/src/main/java/com/example/retrorush/MainActivity.kt
+++ app/src/main/java/com/example/retrorush/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.retrorush
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.retrorush.databinding.ActivityMainBinding
+import com.example.retrorush.models.MovieResponse
+import com.example.retrorush.viewModels.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private var mMovieViewModel: MovieViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Used viewBinding to bind this' activity layout
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        //Setting up the viewModel
+        mMovieViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[MovieViewModel::class.java]
+
+//        setupRecyclerView()
+        setRepositoryObserver()
+    }
+
+    private fun setRepositoryObserver() {
+        mMovieViewModel!!.getMovieRepository()?.observe(this) {
+
+            val movieList: MovieResponse? = it;
+
+        }
     }
 }
